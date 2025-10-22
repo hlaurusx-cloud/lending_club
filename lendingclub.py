1b956c2bd555f491257ab94f349ac83bd71847a8
hlaurusx-cloud <h_laurus_x@pusan.ac.kr>
2025-10-22 22:37:35 +0900

    Add imports for Streamlit, Pandas, and ML libraries
    Added necessary imports for data processing and modeling.

--- lendingclub.py
+++ lendingclub.py
@@ -1,1 +1,9 @@
-
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
