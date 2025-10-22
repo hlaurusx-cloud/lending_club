35657463c007c54a0c1a2386b07acf2e67b7e9ed
hlaurusx-cloud <h_laurus_x@pusan.ac.kr>
2025-10-22 22:41:30 +0900

    Update lendingclub.py

--- lendingclub.py
+++ lendingclub.py
@@ -1,8 +1,7 @@
 import streamlit as st
-# 页面美化
 st.set_page_config(
   page_tilte = "지능형신용평가모형",
-   page_icon="💳",
-    layout="wide",
-    initial_sidebar_state="expanded"
+  page_icon="💳",
+ layout="wide",
+ initial_sidebar_state="expanded"
 )
