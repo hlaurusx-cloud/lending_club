bdf3b54cef0476b68502600578e9676064891b92
hlaurusx-cloud <h_laurus_x@pusan.ac.kr>
2025-10-22 22:40:19 +0900

    Clean up imports in lendingclub.py
    Removed unused imports from lendingclub.py

--- lendingclub.py
+++ lendingclub.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 # 页面美化
 st.set_page_config(
   page_tilte = "지능형신용평가모형",
